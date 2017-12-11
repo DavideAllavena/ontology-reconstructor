18c85e17944e93f460337f9005e062cb2a082373
DavideAllavena <davide.allavena@gmail.com>
2017-12-11 16:15:20 +0100

    Output on stdout and changed Accept header

--- create-onthology.sh
+++ create-onthology.sh
@@ -3,5 +3,5 @@
 for f in $(ls  *.sparql)
 do 
   query=$(cat $f)
-  curl -L -G $1 --data-urlencode query="$query" -H "Content-Type:text/turtle" >> ex-post-ontology.ttl
+  curl -L -G $1 --data-urlencode query="$query" -H "Accept: text/plain" 
 done
